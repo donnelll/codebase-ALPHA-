d845fe21adf70795100917b476c06f66f702e973
Donnell-Lewis <donnell.lewis@coxautoinc.com>
2017-08-14 23:20:11 -0400

    Script that will login to a user's bitbucket account and download all available repositories listed in that user's account. Great for making backups.

---
+++ getAllRepos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Script to get all repositories under a user from bitbucket
+#Usage: getAllRepos.sh [username]
+#source: http://haroldsoh.com/2011/10/07/clone-all-repos-from-a-bitbucket-source/
+
+# 2017-08-14 Don Lewis: Hacked up script to better parse json output since it likes to string together in one
+# line..:/ Also using <user> variable directly and using keys instead of http/https protocol.  Changed API
+# call to reflect actual call to list a user's repositories as original URL did not work. Using "slug"
+# instead of "name" to parse on since slug reveals true repository name without spaces. 
+ 
+#OLD URL:curl -u ${1} https://api.bitbucket.org/1.0/users/${1} > repoinfo
+curl --user dlewis1 https://api.bitbucket.org/1.0/user/repositories > repoinfo
+# cat repoinfo
+ 
+for repo_name in `cat repoinfo | sed -r 's/("slug": )/\n\1/g' | sed -r 's/"slug": "(.*)"/\1/' | sed -e 's/{//' | cut -f1 -d\" | tr '\n' ' '`
+do
+    echo "Cloning " $repo_name
+    #git clone https://dlewis1@bitbucket.org/coxauto/$repo_name
+    git clone git@bitbucket.org:coxauto/$repo_name
+    echo "---"
+done
